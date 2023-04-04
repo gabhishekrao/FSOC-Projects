92ef9806848d8f573fe9833f4e402cebff163edc
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-04 22:01:57 +0530

    Day 1-ending 10 persent done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import {Container, Typography, Grid, Grow, AppBar} from '@mui/material'
 
+import memories from './images/memories.png'
 const App = () => {
     return(
-        <div>
-            <h1>Let's Begin</h1>
-        </div>
+        <Container maxWidth = 'lg'>
+            <AppBar position="static" color="inherit">
+            <Typography variant="h2" align="center">Memories</Typography>
+            <img src={memories} alt="memories" height="600"/>
+            </AppBar>
+        </Container>
     )
 }
 
