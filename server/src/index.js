8a9a0c2bf90700f07e2f867ff5ad9e8d69c4ca3b
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-04 16:10:18 +0530

    created Schema

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import postRouts from './route/posts.js';
+
+const app = express();
+
+app.use('/posts', postRouts)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://gabhishek:GAbhishekPassword@gabhishek01.dq23z80.mongodb.net/SocialMediaApp'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true })
+    .then(() => app.listen(PORT, () => console.log(`Our server is running on PORT: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
