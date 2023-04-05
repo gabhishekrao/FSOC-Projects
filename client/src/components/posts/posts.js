68bdc00a8752a61e070d42191954697a7113268e
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-05 16:20:49 +0530

    day-2 1st commit

--- client/src/components/posts/posts.js
+++ client/src/components/posts/posts.js
@@ -1,1 +1,23 @@
+import React from "react";
+import { useSelector } from "react-redux";
 
+import Post from './Post/Post'
+import useStyles from './style.js'
+import posts from "../../reducers/posts";
+
+const Posts = () => {
+    const post = useSelector((state) => state.posts)
+    const classes = useStyles()
+
+    console.log(posts);
+
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </>
+    )
+}
+
+export default Posts
--- server/src/index.js
+++ server/src/index.js
@@ -3,15 +3,17 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
+
 import postRouts from './route/posts.js';
 
 const app = express();
 
-app.use('/posts', postRouts)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRouts);
 
 const CONNECTION_URL = 'mongodb+srv://gabhishek:GAbhishekPassword@gabhishek01.dq23z80.mongodb.net/SocialMediaApp'
 const PORT = process.env.PORT || 5000;
