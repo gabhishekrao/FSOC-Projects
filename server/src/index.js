81fd114b99fee286a7f3541b379be30e314bcd78
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-06 00:19:29 +0530

    add server too

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 
-import postRouts from './route/posts.js';
+import postRoutes from './route/posts.js';
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRouts);
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://gabhishek:GAbhishekPassword@gabhishek01.dq23z80.mongodb.net/SocialMediaApp'
 const PORT = process.env.PORT || 5000;
