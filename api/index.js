b8c5d61b0a2142b09e978118feec85a479427b31
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-08 23:11:42 +0530

    Setup completed

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config()
+
+const connect = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to Mongoose.");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected.");
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected.");
+});
+
+
+
+
+app.listen(8800, () =>{
+    connect()
+    console.log("We are connected to our Backend!");
+})
