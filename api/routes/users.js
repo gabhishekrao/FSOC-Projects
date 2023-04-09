c3119121d0437bd12d863079056a71faf1f515a2
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-09 18:47:47 +0530

    35 persent done

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,24 +1,33 @@
 import express from 'express';
 import { updateUser, getUser, getUsers, deleteUser } from '../controller/user.js';
-import { verifyToken } from '../utils/verifyToken.js';
+import { verifyToken, verifyUser, verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-        res.send("Hey user, you are logged in.")
-})
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//         res.send("Hey user, you are logged in.")
+// })
+
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//         res.send("Hey user, you are logged in and you can delete your account.")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//         res.send("Hey Admin, you are logged in and you can delete ALL account.")
+// })
 
 //UPDATE
-router.put('/:id', updateUser);
+router.put('/:id',verifyUser, updateUser);
 
 //DELETE
-router.delete('/:id', deleteUser);
+router.delete('/:id',verifyUser, deleteUser);
 
 //GET
-router.get('/:id', getUser);
+router.get('/:id',verifyUser, getUser);
 
 // GET ALL
-router.get('/', getUsers);
+router.get('/',verifyAdmin, getUsers);
 
 
 
